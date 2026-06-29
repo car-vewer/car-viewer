15312065d07ade50df525c843b55b230008cc21f
Syed Najam Ul Hassan Kazmi <k-shassan@c3r4p3.hive.fi>
2026-06-29 16:35:13 +0300

    add specs page and home button

--- addData.go
+++ addData.go
@@ -61,7 +61,7 @@ func makeMain(cars []Car, categories []Category, manufacturers []Manufacturer) [
 }
 
 func getCars() ([]Car, error) {
-	data, err := fetchAPI("/models")
+	data, err := fetchAPI("/models/")
 	if err != nil {
 		return nil, err
 	}
@@ -106,4 +106,23 @@ func getManufacturers() ([]Manufacturer, error) {
 	}
 
 	return manufacturers, nil
+}
+
+
+func getCar(id string) (Car, error) {
+	data, err := fetchAPI("/models/" + id)
+
+	var car Car
+
+	if err != nil {
+		return car, err
+	}
+
+	
+	err = json.Unmarshal(data, &car)
+	if err != nil {
+		return car, err
+	}
+
+	return car, nil
 }
